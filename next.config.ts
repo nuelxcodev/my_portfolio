f7af1523dce3027d2d5ed41b7ba9837e2bff394a
chukwuemeka <chukwuemekacodev@gmail.com>
2025-04-25 18:17:44 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: ".next", // Default value; you can customize it if needed
 };
 
 export default nextConfig;
